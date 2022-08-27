bdfc9cfceab57f2d84540753164e82d99a162660
Daniil Neudakhin <neudakhin.perm@mail.ru>
2022-08-27 10:43:19 +0500

    Выполнение ДЗ №5

--- database/migrations/2022_08_26_141449_create_sources_table.php
+++ database/migrations/2022_08_26_141449_create_sources_table.php
@@ -27,8 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('sources', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('sources');
     }
 };
